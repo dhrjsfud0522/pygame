194e5a57e933759c53cb5150ebbe27dfc3455530
dhrjsfud0522 <codinggunk@gmail.com>
2023-09-17 00:05:02 +0900

    커밋

--- Tower_of_Hanoi.py
+++ Tower_of_Hanoi.py
@@ -18,7 +18,6 @@ s2 = []
 s3 = []
 count = 0
 
-
 class Ring():
     def __init__(self):
         pass
@@ -30,7 +29,7 @@ class Ring():
             Ring().create(screen, n, i, s, RED)
             s += 1
 
-def change_ring(l1, l2, l3, n1, n2, n3): #(자기 자신, 2번째 꺼, 3번쨰 꺼)
+def change_ring(l1, l2, l3, n1, n2, n3):
         global step
         global choise
         global change
@@ -41,27 +40,29 @@ def change_ring(l1, l2, l3, n1, n2, n3): #(자기 자신, 2번째 꺼, 3번쨰 �
                 change = 0
                 step = 1
         else:
-            change = n1
             if choise == n2:
                 if len(l1) == 0:
+                    change = n1
                     l1.insert(len(l1), l2[-1])
                     del(l2[-1])
                     count += 1
                 elif l2[-1] > l1[-1]:
+                    change = n1
                     l1.insert(len(l1), l2[-1])
                     del(l2[-1])
                     count += 1
             if choise == n3:
                 if len(l1) == 0:
+                    change = n1
                     l1.insert(len(l1), l3[-1])
                     del(l3[-1])
                     count += 1
                 elif l3[-1] > l1[-1]:
+                    change = n1
                     l1.insert(len(l1), l3[-1])
                     del(l3[-1])
                     count += 1
-            step = 0
-            
+            step = 0 
         return(l1, l2, l3)
 
 time = 0
@@ -72,15 +73,14 @@ def game_over(screen):
     screen.fill(BACKGROUND)
     font = pygame.font.Font(None, 72)  
     surface = font.render("GMAE OVER!", True, (0, 0, 0))     
-    screen.blit(surface, (270 ,200))
+    screen.blit(surface, (250 ,200))
     surface = font.render("YOU WIN!", True, (0, 0, 0))     
-    screen.blit(surface, (270 ,260))
+    screen.blit(surface, (280 ,260))
+    font = pygame.font.Font(None, 36)  
     surface = font.render("Time: " + str(time), True, (0, 0, 0))     
-    screen.blit(surface, (330 ,320))
+    screen.blit(surface, (330 ,330))
     surface = font.render("Count: " + str(count), True, (0, 0, 0))     
-    screen.blit(surface, (330 ,360))
-
-
+    screen.blit(surface, (330 ,370))
 
 def main():
     pygame.init()
@@ -112,7 +112,6 @@ def main():
                         s2, s1, s3 = change_ring(s2, s1, s3, 2, 1, 3)
                     else:
                         s3, s1, s2 = change_ring(s3, s1, s2, 3, 1, 2)
-                
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_r:
                         s1 = list(range(1, 6))
@@ -132,22 +131,21 @@ def main():
 
             font = pygame.font.Font(None, 36)  
             timer_text = "Time: " + str(time) + "s"
-            timer_surface = font.render(timer_text, True, (0, 0, 0))     
+            timer_surface = font.render(timer_text, True, (0, 0, 0))
             screen.blit(timer_surface, (10 ,10))
 
             count_text = "Count: " + str(count)    
-            count_surface = font.render(count_text, True, (0, 0, 0))     
+            count_surface = font.render(count_text, True, (0, 0, 0))
             screen.blit(count_surface, (150, 10))
     
             min_text = "Min: " + str(2 ** 5 - 1)
-            min_surface = font.render(min_text, True, (0, 0, 0))     
+            min_surface = font.render(min_text, True, (0, 0, 0))
             screen.blit(min_surface, (300, 10))
 
             change_text = str(choise) + " => " + str(change)
-            change_surface = font.render(change_text, True, (0, 0, 0))     
+            change_surface = font.render(change_text, True, (0, 0, 0))
             screen.blit(change_surface, (420, 10)) 
             
-
             pygame.draw.rect(screen, BASE, [0, 500, 800, 600], 0)
             pygame.draw.rect(screen, BASE, [138, 240, 24, 300], 0)
             pygame.draw.rect(screen, BASE, [388, 240, 24, 300], 0)
@@ -162,6 +160,16 @@ def main():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.key == pygame.K_r:
+                        s1 = list(range(1, 6))
+                        s2 = []
+                        s3 = []
+                        choise = 0
+                        change = 0
+                        count = 0
+                        time_m += time
+                        end = 0
         pygame.display.flip()
         clock.tick(60)
     pygame.quit()
